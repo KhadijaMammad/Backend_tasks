cd85e25fefd02518f39512e3b79966e821ccbcc3
Khadija <memmedlixedice318@gmail.com>
2025-09-01 18:01:50 +0400

    mini projects yazildi

--- CMethods/obj/Debug/net8.0/CMethods.AssemblyInfo.cs
+++ CMethods/obj/Debug/net8.0/CMethods.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CMethods")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+78f90dea63a781f086d93139ea77a83f4543c96b")]
 [assembly: System.Reflection.AssemblyProductAttribute("CMethods")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CMethods")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
---
+++ MiniProjects/MiniProjects/Program.cs
@@ -0,0 +1,61 @@
+﻿
+using System;
+
+namespace BankAccountApp
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Atm();
+        }
+        public static void Atm()
+        {
+            int money = 1000;
+            Console.WriteLine("Welcome to the ATM!");
+            Console.WriteLine("Etmek istediyiniz emeliyyati secin: (1: Balans, 2: Pul cekmek, 3: Pul yatirmaq, a: Cixis)");
+            string option = Console.ReadLine();
+
+            if (option == "1")
+            {
+                Console.WriteLine("Balansiniz: " + money);
+                Console.ReadLine();
+            }
+            else if (option == "2")
+            {
+                Console.WriteLine("Ne qeder pul cekmek isteyirsiniz?");
+                int price = Convert.ToInt32(Console.ReadLine());
+                if (price >= money)
+                {
+                    Console.WriteLine("Balansinizda kifayet qeder pul yoxdur");
+                }
+                else if (price <= money)
+                {
+                    money -= price;
+                    Console.WriteLine("Emeliyyat ugurlu oldu. Yeni balansiniz: " + (money - price));
+                }
+                else
+                {
+                    Console.WriteLine("Emeliyyat ugurlu oldu. Yeni balansiniz: " + money);
+                }
+            }
+            else if (option == "3")
+            {
+                Console.WriteLine("Ne qeder pul yatirmaq isteyirsiniz?");
+                int addmoney = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("Emeliyyat ugurlu oldu. Yeni balansiniz: " + (money + addmoney));
+            }
+            else if (option == "a")
+            {
+                Console.WriteLine("ATM-den cixis edilir...");
+                Console.WriteLine("Xeyirle xercleyin");
+                Console.ReadLine();
+            }
+            else
+            {
+                Console.WriteLine("Duzgun emeliyyat secin");
+                Console.ReadLine();
+            }
+        }
+    }
+}
